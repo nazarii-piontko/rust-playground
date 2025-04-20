2a57d3b924f6eafef63d0a72900fb23685347dd9
Nazarii Piontko <1506550+nazarii-piontko@users.noreply.github.com>
2025-04-20 16:47:33 +0200

    Learn lifetime annotation

---
+++ lifetime/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    // Concept #1
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
+
+    // Concept #2
+    let novel = String::from("Hello from struct");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("{}", i.part);
+    
+    // Concept 3
+    let string3 = String::from("Hello World");
+    println!("{}", first_word(string3.as_str()));
+    
+    // Concept 4
+    let s: &'static str = "I have a static lifetime.";
+    println!("{}", s);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
